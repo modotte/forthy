973b9b3e67b541719308515eed65aef4128739ab
modotte <runecroon@gmail.com>
2023-05-02 19:46:23 +0800

    Clean

--- app/Main.hs
+++ app/Main.hs
@@ -5,18 +5,24 @@ module Main (main) where
 import Data.HashMap.Strict qualified as HM
 import Data.List.Split qualified as LS
 import Data.Text qualified as T
-import Relude hiding (Op, Word)
+import Relude hiding (Op, Undefined, Word)
 import Text.Regex.TDFA
 
 data WordDatum = Int
 
-data Word = Com Text | Op Text | Datum WordDatum
+data Word = Com Text | Op Text | Datum WordDatum | Undefined
 
-keywords :: HM.HashMap Text Word
-keywords = HM.fromList [("print", Com "PRINT"), ("+", Op "ADD"), ("*", Op "MULTIPLY")]
+words :: HM.HashMap Text Word
+words = HM.fromList [("print", Com "PRINT"), ("+", Op "ADD"), ("*", Op "MULTIPLY")]
 
 tokenize :: Text -> [[Char]]
 tokenize = LS.splitOn " " . T.unpack
+
+matchToken :: (Eq a, IsString a) => a -> Word
+matchToken "print" = Com "PRINT"
+matchToken "+" = Op "ADD"
+matchToken "*" = Op "MULTIPLY"
+matchToken _ = Undefined
 
 main :: IO ()
 main = do
