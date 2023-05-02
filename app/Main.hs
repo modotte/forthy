54202722156b3efaec8c5bd93f6016a6e2e52e93
modotte <runecroon@gmail.com>
2023-05-02 19:40:23 +0800

    Add tokenize

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,10 @@
 module Main (main) where
 
 import Data.HashMap.Strict qualified as HM
+import Data.List.Split qualified as LS
+import Data.Text qualified as T
 import Relude hiding (Op, Word)
+import Text.Regex.TDFA
 
 data WordDatum = Int
 
@@ -11,6 +14,9 @@ data Word = Com Text | Op Text | Datum WordDatum
 
 keywords :: HM.HashMap Text Word
 keywords = HM.fromList [("print", Com "PRINT"), ("+", Op "ADD"), ("*", Op "MULTIPLY")]
+
+tokenize :: Text -> [[Char]]
+tokenize = LS.splitOn " " . T.unpack
 
 main :: IO ()
 main = do
