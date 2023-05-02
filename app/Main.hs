9ec4d43615062d6f71df0df8f5ecc09605510192
modotte <runecroon@gmail.com>
2023-05-02 19:29:00 +0800

    Add regex-tdfa

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,15 @@
 module Main (main) where
 
 import Data.HashMap.Strict qualified as HM
-import Relude
+import Relude hiding (Op, Word)
+
+data WordDatum = Int
+
+data Word = Com Text | Op Text | Datum WordDatum
+
+keywords :: HM.HashMap Text Word
+keywords = HM.fromList [("print", Com "PRINT"), ("+", Op "ADD"), ("*", Op "MULTIPLY")]
 
 main :: IO ()
 main = do
-  print "Hello world!"
+  pure ()
