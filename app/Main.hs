e15daae85e0fc9742e9a8f676610d5596e349203
modotte <runecroon@gmail.com>
2023-05-07 22:43:10 +0800

    Clean

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Main (main) where
@@ -8,7 +9,11 @@ import Data.Text qualified as T
 import Relude hiding (Op, Undefined, Word)
 import Text.Regex.TDFA
 
-data Word = Com Text | Op Text | Datum Int | Undefined deriving (Show, Eq)
+data Op = Add | Multiply deriving (Show, Eq)
+
+data Comm = Print | Exit deriving (Show, Eq)
+
+data Token = Command Comm | Operator Op | Datum Int | Undefined deriving (Show, Eq)
 
 tokenize :: Text -> [Text]
 tokenize =
@@ -16,17 +21,61 @@ tokenize =
   where
     sp = LS.splitOn " " . T.unpack
 
-parseToken :: Text -> Word
+parseToken :: Text -> Token
 parseToken token
   | token =~ ("[0-9]+" :: Text) =
       maybe Undefined Datum $ readMaybe $ T.unpack token
-  | token == "+" = Op "ADD"
-  | token == "*" = Op "MULTIPLY"
-  | token == "." = Com "PRINT"
+  | token == "+" = Operator Add
+  | token == "*" = Operator Multiply
+  | token == "." = Command Print
+  | token == "bye" = Command Exit
   | otherwise = Undefined
 
-execStack = []
+type ExecStack = [Int]
+
+handleOps :: Op -> ExecStack -> (Int, ExecStack)
+handleOps op execStack = case op of
+  Add ->
+    case execStack of
+      [] -> (0, [])
+      x : xs ->
+        case xs of
+          [] -> (0, [])
+          y : zs -> (x + y, zs)
+  Multiply ->
+    case execStack of
+      [] -> (0, [])
+      x : xs ->
+        case xs of
+          [] -> (0, [])
+          y : zs -> (x + y, zs)
+
+eval :: Text -> ExecStack -> IO ExecStack
+eval text execStack =
+  pure $
+    fmap
+      ( \case
+          Datum x -> x
+          Operator x -> fst $ handleOps x execStack
+          _ -> 0
+      )
+      tokens
+  where
+    tokens = parseToken <$> tokenize text
+
+read :: IO Text
+read =
+  putStr "FORTHY> "
+    >> hFlush stdout
+    >> getLine
 
 main :: IO ()
 main = do
-  print $ parseToken <$> tokenize "1 2 + ."
+  input <- read
+
+  case parseToken input of
+    Command x ->
+      case x of
+        Exit -> pure ()
+        _ -> pure ()
+    _ -> eval input [] >> main
