013c67167fec340225738b1585079ecb107b266e
modotte <runecroon@gmail.com>
2023-05-08 15:56:37 +0800

    Update Stack

--- src/Stack.hs
+++ src/Stack.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE DerivingStrategies #-}
 
-module Stack (Stack (..), mkStack) where
+module Stack (Stack, empty, push, pop) where
 
+import Data.Bifunctor qualified as B
 import Data.Vector (Vector)
 import Data.Vector qualified as V
 
 newtype Stack = Stack {unStack :: Vector Integer} deriving newtype (Show, Eq)
 
-mkStack :: Stack
-mkStack = Stack V.empty
+empty :: Stack
+empty = Stack V.empty
+
+push :: Integer -> Stack -> Stack
+push x (Stack s) = Stack $ V.cons x s
+
+pop :: Stack -> Maybe (Integer, Stack)
+pop (Stack s) =
+  case V.uncons s of
+    Nothing -> Nothing
+    Just x -> Just $ B.second Stack x
