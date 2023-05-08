674d709e5cd5b0fe3de36dccdfffe1c44c14cbe6
modotte <runecroon@gmail.com>
2023-05-08 19:56:51 +0800

    Clean

--- src/Data/Stack.hs
+++ src/Data/Stack.hs
@@ -1,13 +1,14 @@
 module Data.Stack (module Data.Stack, module Data.Stack.Types) where
 
 import Data.Bifunctor qualified as B
+import Data.Stack.Types (Stack (Stack, unStack))
+import Data.Stack.Types qualified as ST
 import Data.Vector qualified as V
 import Relude hiding (empty, state)
 import Relude.Extra qualified as RE
-import Data.Stack.Types
 
 empty :: Stack
-empty = Stack V.empty
+empty = ST.Stack V.empty
 
 addStack :: Integer -> Stack -> Stack
 addStack x = RE.under (V.cons x)
@@ -16,7 +17,7 @@ push :: Integer -> Stack -> Stack
 push x s = s
 
 pop :: Stack -> Maybe (Integer, Stack)
-pop (Stack s) =
+pop (ST.Stack s) =
   case V.uncons s of
     Nothing -> Nothing
-    Just x -> Just $ B.second Stack x
+    Just x -> Just $ B.second ST.Stack x
