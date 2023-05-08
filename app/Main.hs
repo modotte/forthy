5653d992595cafde02fc1cc12b34ea9345bd7253
modotte <runecroon@gmail.com>
2023-05-09 01:25:28 +0800

    Add two ops

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main (main) where
 
+import Control.Monad.Error.Class (MonadError)
 import Data.Stack (Stack)
 import Data.Stack qualified as S
 import Data.Text qualified as T
@@ -26,6 +27,18 @@ tokenize tt
   | tt == "\n" = Right Blank
   | tt == "\t" = Right Blank
   | otherwise = Left "Invalid token"
+
+add :: (MonadState AppState m, MonadError ForthyError m) => m ()
+add = do
+  x <- S.pop
+  y <- S.pop
+  S.push $ x + y
+
+multiply :: (MonadState AppState m, MonadError ForthyError m) => m ()
+multiply = do
+  x <- S.pop
+  y <- S.pop
+  S.push $ x * y
 
 main :: IO ()
 main = do
