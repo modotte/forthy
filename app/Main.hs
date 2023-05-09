693fc3ac8968c521d70e7db94752197542cf0ffd
modotte <runecroon@gmail.com>
2023-05-09 19:53:21 +0800

    Simplify

--- app/Main.hs
+++ app/Main.hs
@@ -4,30 +4,28 @@
 module Main (main) where
 
 import Control.Monad.Error.Class (MonadError)
-import Data.Default.Class (Default (def))
-import Data.Stack (Stack)
+import Data.List.Split qualified as DLS
 import Data.Stack qualified as S
 import Data.Text qualified as T
-import Data.Types (AppState (..), Eff (..), ForthyError, Op (..), Token (..))
+import Data.Types (AppState, Eff, ForthyError, Op, Token)
 import Data.Types qualified as DT
-import Data.Vector qualified as V
 import Relude hiding (Op, Undefined, Word)
 import Text.Regex.TDFA ((=~))
 
 tokenize :: Text -> Token
 tokenize tt
   | tt =~ ("[0-9]+" :: Text) =
-      maybe Blank Datum (readMaybe $ T.unpack tt)
-  | tt == "+" = Operator Add
-  | tt == "*" = Operator Multiply
-  | tt == "dup" = Operator Dup
-  | tt == "." = Effect Print
-  | tt == "bye" = Effect Exit
-  | tt == " " = Blank
-  | tt == "" = Blank
-  | tt == "\n" = Blank
-  | tt == "\t" = Blank
-  | otherwise = Blank
+      maybe DT.Blank DT.Datum (readMaybe $ T.unpack tt)
+  | tt == "+" = DT.Operator DT.Add
+  | tt == "*" = DT.Operator DT.Multiply
+  | tt == "dup" = DT.Operator DT.Dup
+  | tt == "." = DT.Effect DT.Print
+  | tt == "bye" = DT.Effect DT.Exit
+  | tt == " " = DT.Blank
+  | tt == "" = DT.Blank
+  | tt == "\n" = DT.Blank
+  | tt == "\t" = DT.Blank
+  | otherwise = DT.Blank
 
 add :: (MonadState AppState m, MonadError ForthyError m) => m ()
 add = do
@@ -50,9 +48,9 @@ dup = do
 handleOps :: (MonadState AppState m, MonadError ForthyError m) => Op -> m ()
 handleOps =
   \case
-    Add -> add
-    Multiply -> multiply
-    Dup -> dup
+    DT.Add -> add
+    DT.Multiply -> multiply
+    DT.Dup -> dup
 
 ePrint :: (MonadState AppState m, MonadIO m, MonadError ForthyError m) => m ()
 ePrint = do
@@ -62,18 +60,18 @@ ePrint = do
 handleEffs :: (MonadIO m, MonadState AppState m, MonadError ForthyError m) => Eff -> m ()
 handleEffs =
   \case
-    Print -> ePrint
-    Exit -> do
+    DT.Print -> ePrint
+    DT.Exit -> do
       putTextLn "bye!"
       exitSuccess
 
 evalEnv :: (MonadState AppState m, MonadIO m, MonadError ForthyError m) => Token -> m ()
 evalEnv token =
   case token of
-    Datum x -> S.push x
-    Effect eff -> handleEffs eff
-    Operator op -> handleOps op
-    Blank -> pure ()
+    DT.Datum x -> S.push x
+    DT.Effect eff -> handleEffs eff
+    DT.Operator op -> handleOps op
+    DT.Blank -> pure ()
 
 eval :: (MonadIO m, MonadState AppState m, MonadError ForthyError m) => m ()
 eval = do
@@ -87,12 +85,16 @@ eval = do
 runExceptStateT :: s -> StateT s (ExceptT e m) a -> m (Either e (a, s))
 runExceptStateT s = runExceptT . flip runStateT s
 
-instance Default AppState where
-  def :: AppState
-  def = AppState {buffer = ["1", "2", "+", ".", "5", "dup", "+", "."], stack = S.empty, isInCompileMode = False}
+splitText :: Text -> [Text]
+splitText source = T.pack <$> DLS.splitOn " " (T.unpack source)
 
 main :: IO ()
 main = do
-  let source = ""
-  runExceptStateT (def :: AppState) eval >>= print
+  let source =
+        DT.AppState
+          { DT.buffer = splitText " 1 2 + . 5 dup + .",
+            DT.stack = S.empty,
+            DT.isInCompileMode = False
+          }
+  runExceptStateT source eval >>= print
   pure ()
